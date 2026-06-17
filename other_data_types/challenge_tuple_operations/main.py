1f5201bf14180ac8e699227b42ed12ff465c8f2b
mkoliver89 <>
2026-06-17 18:40:23 +0000

    Challenge: Tuple Operations

---
+++ other_data_types/challenge_tuple_operations/main.py
@@ -0,0 +1,23 @@
+# Current inventory on shelf
+shelf = ("apples", "oranges", "bananas", "apples", "grapes", "bananas", "apples")
+#my code
+apple_count = shelf.count("apples")
+print("Number of Apples:", apple_count)
+banana_index = shelf.index("bananas")
+print("First Banana Index:", banana_index)
+#apple_count
+apple_count = shelf.count("apples")
+if apple_count <5:
+ print("Apples need to be restocked.")
+else:
+ print("Apples are sufficiently stocked.")
+grape_count = shelf.count("grapes")
+if grape_count ==1:
+ print("Grapes need to be restocked.", grape_count)
+else:
+ print("Grapes are sufficiently stocked.", grape_count)
+if "oranges" in shelf:
+ orange_index = shelf.index("oranges")
+ print("Oranges are at index:", orange_index)
+else:
+ print("Oranges are out of stock")
